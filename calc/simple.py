70cd750ebfe8bf021f5b2eb34ab54e67baea5711
srcrep <106826286+srcrep@users.noreply.github.com>
2022-06-28 07:09:15 -0500

    Update simple.py

--- calc/simple.py
+++ calc/simple.py
@@ -1,4 +1,52 @@
 import math
+
+/*
+%{
+
+import pandas as pd
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import PySimpleGUI as sg
+import matplotlib
+
+fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
+fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+
+matplotlib.use("TkAgg")
+
+def draw_figure(canvas, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=1)
+    return figure_canvas_agg
+  
+# Define the window layout
+layout = [
+    [sg.Text("Plot test")],
+    [sg.Canvas(key="-CANVAS-")],
+    [sg.Button("Ok")],
+]
+
+# Create the form and show it without the plot
+window = sg.Window(
+    "Matplotlib Single Graph",
+    layout,
+    location=(0, 0),
+    finalize=True,
+    element_justification="center",
+    font="Helvetica 18",
+)
+
+# Add the plot to the window
+draw_figure(window["-CANVAS-"].TKCanvas, fig)
+
+event, values = window.read()
+
+window.close()  
+
+%}
+*/
+  
 P=float(input("Design Pressure:"))
 SE=float(input("Allowable Stress:"))
 D=float(input("Inside Diameter:"))
